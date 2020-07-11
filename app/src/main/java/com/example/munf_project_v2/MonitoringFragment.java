132efb7c8d10944f15b0801e2625a6ff621e5116
Valle-Vh <Valentin_vielhuber@yahoo.de>
2020-07-11 16:31:24 +0200

    Datenbank4.Versuch

--- app/src/main/java/com/example/munf_project_v2/MonitoringFragment.java
+++ app/src/main/java/com/example/munf_project_v2/MonitoringFragment.java
@@ -2,7 +2,6 @@ package com.example.munf_project_v2;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
