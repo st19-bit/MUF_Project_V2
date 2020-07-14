a555ccc835e1f35e2407910c3e18a5d63ac32dff
MaLankes <st19m004@technikum-wien.at>
2020-07-14 18:15:13 +0200

    Neue Startpage

--- app/src/main/java/com/example/munf_project_v2/MUFDatenbank.java
+++ app/src/main/java/com/example/munf_project_v2/MUFDatenbank.java
@@ -3,8 +3,6 @@ package com.example.munf_project_v2;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import com.example.munf_project_v2.SensorDao;
-import com.example.munf_project_v2.Depot;
 
 @Database(entities = {Depot.class},version=1)
 public abstract class MUFDatenbank extends RoomDatabase {
--- app/src/main/java/com/example/munf_project_v2/SensorDao.java
+++ app/src/main/java/com/example/munf_project_v2/SensorDao.java
@@ -6,8 +6,6 @@ import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
-import com.example.munf_project_v2.Depot;
-
 @Dao
 public abstract class SensorDao {
 
